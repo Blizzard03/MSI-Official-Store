8c89cf57f2495faa0a446a1a76dcd854f81cdbd7
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2023-11-25 13:04:54 +0700

    update the project and add some to project

---
+++ src/Models/Custumer/Custumer.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models.Custumer;
+
+/**
+ *
+ * @author Blizzard
+ */
+public class Custumer {
+
+    private String Name, Address, Email, Password;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+}
---
+++ src/Models/Laptop/Bussiners_Producivity/Bussiners_Producivity.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models.Laptop.Bussiners_Producivity;
+
+/**
+ *
+ * @author Blizzard
+ */
+public class Bussiners_Producivity {
+  private String models,cpu,gpu;  
+    private double price;
+}
